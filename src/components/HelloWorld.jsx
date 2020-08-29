8c1bf7715a74a318c80083f322ca404cc5238b9b
Andres ayala <rattman.c@gmail.com>
2020-08-28 22:01:07 -0500

    Header terminado y barra de busqueda

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import '../assets/styles/App.scss'
 
 const HelloWorld = () =>(
     <h1>Hello World</h1>
---
+++ src/components/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/styles/components/Search.scss'
+
+const Search = ()=>(
+    <section className="main">
+    <h2 className="main_title">¿Que quieres ver hoy?</h2>
+    <input type="text" className="input" name="" id="" placeholder="Buscar..."/>
+    <h2 className="categories__title">Mi lista</h2>
+</section>
+
+)
+export default Search
