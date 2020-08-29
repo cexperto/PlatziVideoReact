6c29ad31afbc7964256eb167bae23be2964ff09c
Andres ayala <rattman.c@gmail.com>
2020-08-29 05:30:57 -0500

    Actualizacion de notas, webpack y sass

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import '../assets/styles/components/CarouselItem.scss'
-
+import img1 from '../assets/static/pexels-angelo-duranti-3512848.jpg'
 const CarouselItem = () =>(
     <div className="carousel-item">
-    <img className="carousel-item__img"src="img/5.jpg"alt="pc"/>
+    <img className="carousel-item__img"src={img1} alt="pc"/>
      <div className="carousel-item__details">
         <div className="carousel-item__details-img">
          <img className="carousel-item__details--play"src="https://img.icons8.com/flat_round/64/000000/play--v1.png"/>
