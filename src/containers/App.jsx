354f84945fe62773a8ef0327b6ca5bdded399b09
Andres ayala <rattman.c@gmail.com>
2020-08-31 03:50:29 -0500

    Custom Hooks funcionando

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,24 +5,18 @@ import Categories from '../components/Categories'
 import Carousel from '../components/Carousel'
 import CarouselItem from '../components/CarouselItem'
 import Footer from '../components/Footer'
+import useInitialState from '../hooks/useInitialState.js'
 import '../assets/styles/App.scss'
+
+const API = 'http://localhost:3000/initialState/'
+
 const App = ()=>{
-    const [videos, setVideos] = useState({
-        'mylist':[],
-        'trends':[],
-        'originals':[]
-    })
-    useEffect(()=>{
-        fetch('http://localhost:3000/initialState/')
-        .then(response => response.json())
-        .then(data =>setVideos(data))
-    },[])
-    
+    const initialState = useInitialState(API)
     return(
         <div className="App">
             <Header />
             <Search />
-            {videos.mylist.length > 0 && 
+            {initialState.mylist.length > 0 && 
                 <Categories title="Mi lista"> 
                 <Carousel>
                     <CarouselItem/>
@@ -31,7 +25,7 @@ const App = ()=>{
             }
             <Categories title="Tendencias"> 
                 <Carousel>
-                    {videos.trends.map(item =>
+                    {initialState.trends.map(item =>
                         <CarouselItem key={item.id}{...item}/>
                     )}
                 </Carousel>
@@ -39,11 +33,10 @@ const App = ()=>{
             </Categories>
             <Categories title="Originales de platzi video"> 
                 <Carousel>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <CarouselItem/>
+                    {initialState.originals.map(item =>
+                        <CarouselItem key={item.id}{...item} />
+                    )}
                 </Carousel>
-
             </Categories>
             <Footer/>
         </div>
