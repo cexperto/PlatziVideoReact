6c973e7a07d9e20cac1d180aa2e2382b95fd43bf
Andres ayala <rattman.c@gmail.com>
2020-08-30 19:04:38 -0500

    Conectando con hooks a un api fake

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import '../assets/styles/components/CarouselItem.scss'
 import img1 from '../assets/static/pexels-angelo-duranti-3512848.jpg'
-const CarouselItem = () =>(
+const CarouselItem = ({ cover, title, year, contentRating, duration}) =>(
     <div className="carousel-item">
-    <img className="carousel-item__img"src={img1} alt="pc"/>
+    <img className="carousel-item__img"src={cover} alt={title}/>
      <div className="carousel-item__details">
         <div className="carousel-item__details-img">
          <img className="carousel-item__details--play"src="https://img.icons8.com/flat_round/64/000000/play--v1.png"/>
          <img className="carousel-item__details--suma"src="https://img.icons8.com/flat_round/64/000000/plus.png"/>
       </div> 
-     <p className="carousel-item__details--title">Titulo descriptivo</p>
-     <p className="carousel-item__details--subtitle">2019 16+ 114 minutos</p>
+<p className="carousel-item__details--title">{title}</p>
+     <p className="carousel-item__details--subtitle">
+     {`${year} ${contentRating} ${duration}`}
+     </p>
  </div>
  </div>
 )
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React ,{ useState, useEffect} from 'react'
 import Header from '../components/Header'
 import Search from '../components/Search'
 import Categories from '../components/Categories'
@@ -6,20 +6,47 @@ import Carousel from '../components/Carousel'
 import CarouselItem from '../components/CarouselItem'
 import Footer from '../components/Footer'
 import '../assets/styles/App.scss'
-const App = ()=>(
-<div className="App">
-    <Header />
-    <Search />
-    <Categories>
-        <Carousel>
-            <CarouselItem/>
-            <CarouselItem/>
-            <CarouselItem/>
-            <CarouselItem/>
-        </Carousel>
+const App = ()=>{
+    const [videos, setVideos] = useState({
+        'mylist':[],
+        'trends':[],
+        'originals':[]
+    })
+    useEffect(()=>{
+        fetch('http://localhost:3000/initialState/')
+        .then(response => response.json())
+        .then(data =>setVideos(data))
+    },[])
+    
+    return(
+        <div className="App">
+            <Header />
+            <Search />
+            {videos.mylist.length > 0 && 
+                <Categories title="Mi lista"> 
+                <Carousel>
+                    <CarouselItem/>
+                </Carousel>
+            </Categories>
+            }
+            <Categories title="Tendencias"> 
+                <Carousel>
+                    {videos.trends.map(item =>
+                        <CarouselItem key={item.id}{...item}/>
+                    )}
+                </Carousel>
 
-    </Categories>
-    <Footer/>
-</div>
-)
+            </Categories>
+            <Categories title="Originales de platzi video"> 
+                <Carousel>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                </Carousel>
+
+            </Categories>
+            <Footer/>
+        </div>
+        )
+} 
 export default App
